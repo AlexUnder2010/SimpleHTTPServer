c86491ecf009b4286139f841b1f484d36c8432f9
Alex Mikhailik <alex-under-2010@mail.ru>
2018-01-10 23:56:13 +0200

    Initial commit

---
+++ ConsoleApplication4/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Enter path to site (ex. D:\site):");
+            string x = Console.ReadLine();
+            string myFolder = x;
+            SimpleHTTPServer myServer = new SimpleHTTPServer(myFolder, 80);
+            Console.WriteLine("Server is running on this port: " + myServer.Port);
+            Console.ReadLine();
+        }
+    }
+}
